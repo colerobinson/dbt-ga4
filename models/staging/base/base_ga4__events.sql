52c059acad2ddb7a9ee31658455b8708a75e4aee
colerobinson <crobinson@finishline.com>
2024-05-07 13:59:30 -0400

    multi id as raw view

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -5,7 +5,6 @@
 
 {{
     config(
-        pre_hook="{{ ga4.combine_property_data() }}" if var('combined_dataset', false) else "",
         materialized = 'incremental',
         incremental_strategy = 'insert_overwrite',
         partition_by={
@@ -20,7 +19,7 @@
 with source as (
     select
         {{ ga4.base_select_source() }}
-    from {{ source('ga4', 'events') }}
+    from {{ ref("raw_ga4_union") }}
     where cast(left(replace(_table_suffix, 'intraday_', ''), 8) as int64) >= {{var('start_date')}}
     {% if is_incremental() %}
         and parse_date('%Y%m%d', left(replace(_table_suffix, 'intraday_', ''), 8)) in ({{ partitions_to_replace | join(',') }})
---
+++ models/staging/source/raw_ga4_union.sql
@@ -0,0 +1,21 @@
+{{
+    config(materialized = "view")
+}}
+
+{% set property_ids = var('property_ids') %}
+{% set source_project = var('source_project') %}
+
+{% if property_ids | length > 1 %}
+    {% set queries = [] %}
+    {% for property_id in property_ids %}
+        {% set query = "SELECT * FROM " ~ '`' source_project ~ '.analytics_' ~ property_id ~ '.events_*' ~ "`" %}
+        {% do queries.append(query) %}
+    {% endfor %}
+    
+    SELECT * FROM (
+        {{ queries | join(' UNION ALL ') }}
+    )
+{% else %}
+    SELECT * FROM {{ source_project }}.analytics_{{ property_ids[0] }}.events_*
+{% endif %}
+
